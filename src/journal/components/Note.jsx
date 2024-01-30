35a2b4ec3dc08d88088f52fb6e4f05ed17aed0e2
CarlosArancibia <arancibiacarlos.b@gmail.com>
2024-01-29 21:44:47 -0400

    note style fixed

--- src/journal/components/Note.jsx
+++ src/journal/components/Note.jsx
@@ -18,20 +18,17 @@ export const Note = ({ title, description, date, id, photosURL }) => {
   };
 
   return (
-    <article
-      className="p-3 h-28 rounded-md mt-1 mx-2 hover:bg-[#333] cursor-pointer"
-      onClick={onSelectNote}
-    >
+    <article className="p-3  rounded-md mt-1 mx-2 hover:bg-[#333] cursor-pointer" onClick={onSelectNote}>
       <div className="flex w-full items-center">
-        <div className="note-item">
+        <div className="note-item min-h-12">
           <h3 className="font-semibold text-sm text-ellipsis overflow-hidden whitespace-nowrap">{title}</h3>
           <p className="text-sm font-thin text-ellipsis overflow-hidden whitespace-nowrap">{description}</p>
         </div>
         {photosURL.length > 0 && (
-          <img className="max-w-16 h-16 object-scale-down" src={photosURL[0]} alt="image-post" />
+          <img className="max-w-14 rounded-md h-14 object-scale-down" src={photosURL[0]} alt="image-post" />
         )}
       </div>
-      <span className="block text-xs text-gray-400">{`${new Date(date).toLocaleTimeString()}`}</span>
+      <span className="block text-xs  text-gray-400">{`${new Date(date).toLocaleTimeString()}`}</span>
     </article>
   );
 };
